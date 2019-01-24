32052d133ecbe5ff4235893928684731cd1a33b3
Clinton <Danielsc05@gmail.com>
2019-01-24 18:29:16 -0500

    Updates

---
+++ precache-manifest.794910fc6206f32ee2797b291dceaf96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eebd5131cf9b24a5471e",
+    "url": "/CDaniels417/memory-game/static/css/main.18e4c2e4.chunk.css"
+  },
+  {
+    "revision": "eebd5131cf9b24a5471e",
+    "url": "/CDaniels417/memory-game/static/js/main.eebd5131.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/CDaniels417/memory-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "96b048755baa2d8df4d9",
+    "url": "/CDaniels417/memory-game/static/js/runtime~main.96b04875.js"
+  },
+  {
+    "revision": "b89769d226dc98a9c3d4a256af113616",
+    "url": "/CDaniels417/memory-game/static/media/Mario-Kart-8-Deluxe.b89769d2.jpg"
+  },
+  {
+    "revision": "2d9f724b187058811f5972bc3b5c3f1f",
+    "url": "/CDaniels417/memory-game/index.html"
+  }
+];
